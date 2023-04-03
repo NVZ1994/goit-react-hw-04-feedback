817c9da32a66e4b2b98f1026c96f2ff0c8622ff6
NVZ1994 <nikita.zhuk.1994@gmail.com>
2023-04-03 21:02:13 +0300

    cursor pointer

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,27 +1,28 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 export const ButtonsSection = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px;
 
-    button {
-        color: blue;
-        width: 100px;
-        padding: 10px;
-        border: none;
-        outline: none;
-        text-transform: uppercase;
-        background-color: #ffff49;
+  button {
+    color: blue;
+    width: 100px;
+    padding: 10px;
+    border: none;
+    outline: none;
+    text-transform: uppercase;
+    background-color: #ffff49;
+    cursor: pointer;
 
-    :first-of-type{
-        background-color: #3be93b;
-        color: white;
-    }  
-    :last-of-type{
-        background-color: red;
-        color: green
-    }  
+    :first-of-type {
+      background-color: #3be93b;
+      color: white;
     }
-`
+    :last-of-type {
+      background-color: red;
+      color: green;
+    }
+  }
+`;
